8f49556cfbd58a37ead661e216ba68446ccbe9a0
Musfriah Murtaza Korai <musfirahkorai@gmail.com>
2024-07-05 11:14:36 +0500

    first commit

---
+++ first.js
@@ -0,0 +1,9 @@
+console.log("Musfira Murtaza");
+// let num1 = 1
+// let num2 = 2
+// console.log(num1);
+// console.log(num2);
+function sum(a, b) {
+    return a + b;
+}
+sum(1, 2);
---
+++ first.ts
@@ -0,0 +1,12 @@
+console.log("Musfira Murtaza")
+
+// let num1 = 1
+// let num2 = 2
+// console.log(num1);
+// console.log(num2);
+
+
+function sum(a:number,b:number){
+    return a+b
+}
+console.log(sum(5,10));
