182a75059d9cc9ce77c72e5812a419a21138975a
Musfirah Murtaza Korai <161106882+musfirahkorai@users.noreply.github.com>
2024-07-08 12:00:46 +0500

    Add files via upload

---
+++ seventh.js
@@ -0,0 +1,13 @@
+// Function
+// Function Invocation means calling Function
+// To execute a function, you simply invoke it by using its name
+// followed by parentheses.
+// Function of a company that recently hired
+// new people and welcoming them
+//Function Dfine
+
+function greet(name, id) {
+    console.log("Welcome, ".concat(name, " and your id is ").concat(id));
+}
+//Function Invocation (Call)
+greet("musfira", 1);
---
+++ seventh.ts
@@ -0,0 +1,19 @@
+// Function
+
+// Function Invocation means calling Function
+
+// To execute a function, you simply invoke it by using its name
+// followed by parentheses.
+
+// Function of a company that recently hired
+// new people and welcoming them
+
+//Function Dfine
+
+function greet(name:string, id:number){
+     console.log(`Welcome, ${name} and your id is ${id}`);
+     
+}
+
+//Function Invocation (Call)
+greet("musfira",1);
